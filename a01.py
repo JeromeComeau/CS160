085cc9ab6fc576e3f2448929e080b52fd2b5f947
Jerome Comeau <jerome.comeau@gmail.com>
2018-04-18 20:26:11 -0700

    Class assignments

---
+++ a01.py
@@ -0,0 +1,14 @@
+'''This program was written by Jerome COmeau.
+
+It prints some text to the user.
+Date Created: 04/12/2018 '''
+
+#Print text to the user
+print("Hello!! My name is Jerome Comeau.")
+
+#input statement to read name from the user
+name = input("What is yours?   ")
+
+print("Hello ", name, "!!  I work as a Database Administrator.")
+print("I have been working in Tech for 20 years, and I enjoy riding a motorcycle!")
+print("Hello World!!!")
---
+++ hello.py
@@ -0,0 +1,12 @@
+'''This program was written by Jerome Comeau
+CS-160-0-20629
+Created 2018-04-04'''
+
+
+print('Hello, My Name is Jerome.')
+#input statement to read name from user
+name = input("What is your name? ")
+print('Hello, ', name, '!')
+print('I am a student at PCC.')
+print('I have been working in tech for 20 years.')
+print('Hello, World!')
